081d2344a23310b2751dceeb12f2c9972d6fc00f
Alexander <AlexanderTurchin1@gmail.com>
2020-07-20 23:36:13 +0300

    Create reusable block.

--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Col, Row} from 'reactstrap';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
+import RowBlock from '../rowBlock';
 
 class CharacterPage extends React.Component {
     
@@ -34,22 +34,23 @@ class CharacterPage extends React.Component {
             );
         }
 
+        const itemList = (
+            <ItemList 
+                onCharSelected={this.onCharSelected}
+                getData={this.gotService.getAllCharacters}
+                renderItem={({name, gender}) => `${name} (${gender})`}
+            />
+        );
+
+        const charDetails = (
+            <CharDetails 
+                charId={this.state.selectedChar}
+            />
+        );
+
         return (
-            <Row>
-                <Col md='6'>
-                    <ItemList 
-                        onCharSelected={this.onCharSelected}
-                        getData={this.gotService.getAllCharacters}
-                        renderItem={({name, gender}) => `${name} (${gender})`}
-                    />
-                </Col>
-                <Col md='6'>
-                    <CharDetails 
-                        charId={this.state.selectedChar}
-                    />
-                </Col>
-            </Row>
-        )
+            <RowBlock left={itemList} right={charDetails}/>
+        );
     }
 }
 
